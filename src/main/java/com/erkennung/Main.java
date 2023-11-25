835727acc56c47f4066fe241c2bf4ffe33892279
Harry Geoffrey Trebing <hgmtrebing@gmail.com>
2023-11-25 18:27:39 -0500

    Addition of Person enum, as well as some testing code in Main.

--- src/main/java/com/erkennung/Main.java
+++ src/main/java/com/erkennung/Main.java
@@ -1,7 +1,36 @@
 package com.erkennung;
 
+import com.erkennung.deutsch.grammar.ArticleForm;
+import com.erkennung.deutsch.grammar.Articles;
+import com.erkennung.deutsch.grammar.constants.ArticleType;
+import com.erkennung.deutsch.grammar.constants.Case;
+import com.erkennung.deutsch.grammar.constants.Gender;
+import com.erkennung.deutsch.grammar.constants.Number;
+
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Articles a = new Articles();
+
+        ArticleForm form = a.getArticle(Case.NOMINATIVE, Gender.FEMININE, Number.SINGULAR, ArticleType.INDEFINITE);
+        System.out.println("Word: " + form.getWord());
+        System.out.println("Case: " + form.getGrammaticalCase());
+        System.out.println("Gender: " + form.getGender());
+        System.out.println("Number: " + form.getNumber());
+        System.out.println("Type: " + form.getArticleType());
+        /*
+        Set<ArticleForm> filtered = a.filter(null, null, null, null);
+        System.out.println(filtered.size());
+        for (ArticleForm form: filtered) {
+            System.out.println("--------------------");
+            System.out.println("Word: " + form.getWord());
+            System.out.println("Case: " + form.getGrammaticalCase());
+            System.out.println("Gender: " + form.getGender());
+            System.out.println("Number: " + form.getNumber());
+            System.out.println("Type: " + form.getArticleType());
+        }
+
+         */
     }
 }
---
+++ src/main/java/com/erkennung/deutsch/grammar/constants/Person.java
@@ -0,0 +1,7 @@
+package com.erkennung.deutsch.grammar.constants;
+
+public enum Person {
+    FIRST_PERSON,
+    SECOND_PERSON,
+    THIRD_PERSON
+}
