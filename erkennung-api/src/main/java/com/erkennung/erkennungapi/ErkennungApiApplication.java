f0adb3376b56d77e011a94bdc5450d1f0ec11547
Harry Geoffrey Trebing <hgmtrebing@gmail.com>
2023-11-25 19:01:10 -0500

    Addition of some code that was accidentally uncommitted in new `erkennung-api` module.

---
+++ erkennung-api/src/main/java/com/erkennung/erkennungapi/ErkennungApiApplication.java
@@ -0,0 +1,13 @@
+package com.erkennung.erkennungapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErkennungApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErkennungApiApplication.class, args);
+	}
+
+}
